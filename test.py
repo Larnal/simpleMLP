f88b28ed01d98b95233772edc848322a3623dd91
Jerome Bruzaud <reeloyd@hotmail.com>
2018-12-18 19:23:38 +0100

    cleaning

--- test.py
+++ test.py
@@ -2,29 +2,31 @@ import matplotlib.pyplot as plt
 from neuralnetwork import *
 import time
 
-print("lo")
-# donnees a apprendre:
-x = normalize(np.load('basetrain.npy'))
+""" ########################  Données ##################################"""
+x = np.load('basetrain.npy')
 labels = np.load('labeltrain.npy')
-xtest = normalize(np.load('basetest.npy'))
+xtest = np.load('basetest.npy')
 labeltest = np.load('labeltest.npy')
 
 target = label2target(labels)
-testtarget = label2target(labeltest)
+""" ########################  Parametres d’apprentissage ###############"""
 
-# parametres d’apprentissage:
-c = 16  # Nombre de perceptrons couche cachée
-w1, w2 = mlp2def(np.size(x, 0), c, np.size(target, 0))  # pour MLP 2 couches
-w = mlp1def(np.size(x, 0), np.size(target, 0))  # pour MLP 1 couche
-it_train = 10
-epoch = 10
-lr = 0.005
-qerror = np.empty([1], dtype=float)
 
-start = time.time()  # Start time measurement
+n = np.size(x, 0)                                           # Nb element dans sample
+m = np.size(target, 0)                                      # Nb de classe
+c = 80                                                      # Nombre de perceptrons couche cachée
 
-# Entrainement pour MLP 1 couche
+it_train = 2                                                # Nb d'itérations d'apprentissage (dans un cycle)
+epoch = 50                                                  # Nb d'epoch (test sur base de test)
+lr = 0.05                                                   # Learning rate
+
+
+qerror = np.empty([1], dtype=float)                         # vecteur d'erreur quadratique
+start = time.time()                                         # Mesure du temps d'exécution
+
+""" ########################  Entrainement MLP 1 couche ###############"""
 # print("Apprentissage MLP1 ...")
+# w = mlp1def(np.size(x, 0), np.size(target, 0))  # pour MLP 1 couche
 # Lscore = np.asarray([list(score(mlpclass(mlp1run(x, w)), labels))])
 # Gscore = np.asarray([list(score(mlpclass(mlp1run(xtest, w)), labeltest))])
 # for i in range(epoch):
@@ -36,8 +38,9 @@ start = time.time()  # Start time measurement
 #     Gscore = np.append(Gscore, [list(score(mlpclass(mlp1run(xtest, w)), labeltest))], axis=0)
 
 
-# Entrainement pour MLP 2 couches
+""" ########################  Entrainement MLP 2 couches (1 cachée) ###############"""
 print("Apprentissage MLP2 ...")
+w1, w2 = mlp2def(np.size(x, 0), c, np.size(target, 0))      # pour MLP 2 couches
 Lscore = np.asarray([list(score(mlpclass(mlp2run(x, w1, w2)), labels))])
 Gscore = np.asarray([list(score(mlpclass(mlp2run(xtest, w1, w2)), labeltest))])
 for i in range(epoch):
@@ -48,9 +51,14 @@ for i in range(epoch):
     Lscore = np.append(Lscore, [list(score(mlpclass(mlp2run(x, w1, w2)), labels))], axis=0)
     Gscore = np.append(Gscore, [list(score(mlpclass(mlp2run(xtest, w1, w2)), labeltest))], axis=0)
 
+""" ########################  FIN SECTION ENTRAINEMENT ###############"""
+
+
+
 time = time.time() - start
 
-# affichage erreur quadratique:
+""" ########################  Affichage ###############################"""
+# affichage erreur quadratique
 axe_x = np.linspace(1, it_train * epoch, it_train * epoch)
 plt.plot(axe_x, qerror[1:])
 plt.ylabel('Cout quadratique')
@@ -68,7 +76,7 @@ plt.legend(('Score d\'apprentissage', 'Score de généralisation'),
            loc='lower right')
 plt.show()
 
-# affichage Taux d'erreur:
+# affichage Taux de réussite / apprentissage:
 plt.plot(axe_x, Lscore[:, 1], 'r--', axe_x, Gscore[:, 1], 'b--')
 plt.title('Taux d\'apprentissage')
 plt.xlabel("%")
@@ -76,4 +84,7 @@ plt.xlabel("Iterations de test")
 plt.legend(('Apprentissage', 'Généralisation'),
            loc='lower right')
 plt.show()
-print("finished in ", time, "s !")
+print("exécuté en ", round(time, 0), "s !")
+print("Taux de reussite sur base d'entrainement = ", Lscore[-1,1])
+print("Taux de reussite sur base de test = ", Gscore[-1,1])
+
